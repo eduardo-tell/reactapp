7b2147e70759f148366d36115dfe86def8b51c53
Eduardo Tell <eduardo-tell@hotmail.com>
2020-05-03 19:16:27 -0300

    Adiciona fontes e estilo para conteúdo do Início

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './styles.scss';
 
-window.onscroll = scroll;
+if(window.onscroll) scroll();
+
+window.addEventListener('scroll', scroll, true)
 document.querySelectorAll('.open-menu')[0].addEventListener('click', alterMenu, true)
 document.querySelectorAll('.menu-itens__close')[0].addEventListener('click', alterMenu, true)
+document.querySelectorAll('.main')[0].onclick = () => { if(document.querySelector('.main.blur')) alterMenu() }
 
-function alterMenu(){  
+function alterMenu(){
     document.querySelectorAll('.menu-itens')[0].classList.toggle('open')
     document.querySelectorAll('.main')[0].classList.toggle('blur')
 }
@@ -18,7 +21,6 @@ function scroll(){
         header.classList.remove('scroll')
     }
 };
-
 
 export default class Header extends Component {
     render(){
--- my-app/src/pages/home/index.js
+++ my-app/src/pages/home/index.js
@@ -1,16 +1,55 @@
 import React, { useState } from 'react'
 import Header from '../../components/Header';
 import Button from '../../components/Button';
+import './style.scss';
+
+// Images
+import Telephone from '../../assets/telephone.png';
+import Dashboard from '../../assets/dashboard.png';
 
 export default function Home() {
     return (
         <>
             <Header />
-            <section className="container">
-                <section className="apresentation text-center">
-                    <h1> Hello World </h1>
-                    <Button label='Teste' />
-                </section>
+            <section className="section-01">
+                <div className="container">
+                    <section className="apresentation">
+                        <div><h1> A melhor técnologia de ponta que você pode ter. </h1></div>
+                        <div> <figure> <img src={Telephone} alt="Celular" /> </figure> </div>
+                    </section>
+                </div>
+            </section>
+
+            <section className="section-02">
+                <div className="container">
+                    <header>
+                        <ul>
+                            <li className="ativo"> <a href=""> Lorem impsum </a> </li>
+                            <li> <a href=""> Lorem impsum </a> </li>
+                            <li> <a href=""> Lorem impsum </a> </li>
+                            <li> <a href=""> Lorem impsum </a> </li>
+                        </ul>
+                    </header>
+                    <article id="article-01">
+                        <div className="contents">
+                            <div>
+                                <figure>
+                                    <img src={Dashboard} alt="" />
+                                </figure>
+                            </div>
+                        </div>
+                    </article>
+                    <article id="article-02">
+                        <div className="contents">
+                            <div></div>
+                        </div>
+                    </article>
+                    <article id="article-03">
+                        <div className="contents">
+                            <div></div>
+                        </div>
+                    </article>
+                </div>
             </section>
         </>
     );
